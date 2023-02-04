30cbf900f02c89232b6ddcd51b266dba2590ac58
gopps95 <shankargopi7@gmail.com>
2023-02-04 12:33:38 +0530

    Added new1

--- scrap.py
+++ scrap.py
@@ -7,3 +7,11 @@ print(page)
 print(page.content)
 soup = BeautifulSoup(page.content,'html.parser')
 lists = soup.find_all('selection',class_="listing-search-item")
+print(lists)
+for list in lists:
+    title = lists.find('a',class_="listing-search-item__link--title").text.replace('\n', '')
+    price = lists.find('a',class_="listing-search-item__price").text.replace('\n', '')
+    location = list.find('div', class_="listing-search-item__location").text.replace('\n', '')
+    area = list.find('span', class_="illustrated-features__description").text.replace('\n', '')
+    info = [title, price, location,  area]
+    print(info)
