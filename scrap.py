91fb23d0d38ed270a7863d69f1745f416ac19e72
gopps95 <shankargopi7@gmail.com>
2023-02-04 12:31:31 +0530

    Added new

--- scrap.py
+++ scrap.py
@@ -1,3 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 from csv import writer
+url = "https://www.pararius.com/apartments/den-haag"
+page = requests.get(url)
+print(page)
+print(page.content)
+soup = BeautifulSoup(page.content,'html.parser')
+lists = soup.find_all('selection',class_="listing-search-item")
