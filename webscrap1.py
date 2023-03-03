0a265f6dae830293091c276a12570fb908df4821
gopps95 <shankargopi7@gmail.com>
2023-03-03 22:41:08 +0530

    new github image scraper

---
+++ webscrap1.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup as bs
+github_user = input('Enter the GitHub User')
+url = 'https://hithub.com'+github_user
+r = requests.get(url)
+soup = bs(r.content, 'html.parser')
+profile_image = soup.find('img',{'alt': 'Avatar'})['src']
+print(profile_image)
