8a01eceed54df826a99d9079dec9569de58745e8
gopps95 <shankargopi7@gmail.com>
2023-03-10 22:06:28 +0530

    Intail commit

---
+++ webscrap_2.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+ 
+options = Options()
+options.add_experimental_option("detach",True)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+driver.get("https://www.uxmatters.com/")
