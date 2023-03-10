ae82d05afab6cc354c31a2c86c078df5be0d2219
gopps95 <shankargopi7@gmail.com>
2023-03-10 22:12:34 +0530

    collected the links

--- webscrap_2.py
+++ webscrap_2.py
@@ -9,3 +9,10 @@ options.add_experimental_option("detach",True)
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
 driver.get("https://www.uxmatters.com/")
+driver.maximize_window()
+
+
+links = driver.find_elements("xpath","//a[@href]")
+
+for link in links:
+    print(links)
