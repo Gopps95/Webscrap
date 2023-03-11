758d5e73681e1fdb7ce2365e75dbebecb9e02e6d
gopps95 <shankargopi7@gmail.com>
2023-03-11 21:17:17 +0530

    issue in fetching the link

--- webscrap_2.py
+++ webscrap_2.py
@@ -15,7 +15,13 @@ driver.maximize_window()
 links = driver.find_elements("xpath","//a[@href]")
 
 for link in links:
-    if "style_books__24wOD" in link.get_attribute("innerHTML"):
+    if "style_books_col__3U9Vo" in link.get_attribute("innerHTML"):
         link.click()
         break
-#book_links  = driver.find_elements("xpath","//")
+
+
+book_links = driver.find_elements("xpath",
+                                  "//div[contains(@class,'style_books_col__3U9Vo')][count(.//a)=1]//a")
+
+for book_link in book_links:
+    print(book_link.get_attribute("innerHTML"))
