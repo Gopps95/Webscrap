f0e8c1c08c5cd2e3ea3e5433cccd61a8225a5c4c
gopps95 <shankargopi7@gmail.com>
2023-03-11 22:03:53 +0530

    error switching windos

--- webscrap_2.py
+++ webscrap_2.py
@@ -2,8 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException
 
- 
 options = Options()
 options.add_experimental_option("detach",True)
 
@@ -11,17 +14,15 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),optio
 driver.get("https://www.robinsharma.com/")
 driver.maximize_window()
 
-
 links = driver.find_elements("xpath","//a[@href]")
 
-for link in links:
-    if "style_books_col__3U9Vo" in link.get_attribute("innerHTML"):
-        link.click()
-        break
+book_picture = driver.find_element(By.XPATH,'//img[@alt="Robin Sharma"]')
+book_picture.click()
 
+try:
+    wait = WebDriverWait(driver, 10)
+    new_window = wait.until(EC.new_window_is_opened)
+    driver.switch_to.window(new_window)
+except WebDriverException as e:
+    print(f"Error switching to new window: {e}")
 
-book_links = driver.find_elements("xpath",
-                                  "//div[contains(@class,'style_books_col__3U9Vo')][count(.//a)=1]//a")
-
-for book_link in book_links:
-    print(book_link.get_attribute("innerHTML"))
